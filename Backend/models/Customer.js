7f8de339a303946a78c78ab5a6cd956377d86618
Hira Muazzam <jahidsohana@icloud.com>
2023-10-09 19:10:17 +0500

    validation

--- Backend/models/Customer.js
+++ Backend/models/Customer.js
@@ -58,7 +58,6 @@ const customerSchema = new mongoose.Schema({
     match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
     validate: {
         validator: function (value) {
-          // Custom email validation (you can add more specific rules if needed)
           return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
         },
         message: 'Invalid email address format.',
