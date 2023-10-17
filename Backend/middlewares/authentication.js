0a9b26f2237b06fadc71788373612da397d4e426
Hira Muazzam <jahidsohana@icloud.com>
2023-10-17 19:36:34 +0500

    error handling and adding authentication middleware

--- Backend/middlewares/authentication.js
+++ Backend/middlewares/authentication.js
@@ -4,15 +4,17 @@ require('dotenv').config();
 
 // Middleware function to authenticate token
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'] ;
+    var token = req.headers['authorization'] ;
   
     if (!token) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
   
+    token = token.split(" ")[1];
 
     jwt.verify(token, process.env.SECRETKEY, (err, user) => {
         if (err) {
+            console.log(err.message)
             return res.status(403).json({ message: 'Forbidden' });
         }
         req.user = user;
@@ -21,3 +23,5 @@ const authenticateToken = (req, res, next) => {
     });
   };
   
+
+  module.exports=authenticateToken;
