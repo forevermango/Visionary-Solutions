cdb4c09f468bc90326c96307da2bb8bbdd518f06
Hira Muazzam <jahidsohana@icloud.com>
2023-10-26 01:10:00 +0500

    frontend: calling login api

--- Frontend/src/views/login.vue
+++ Frontend/src/views/login.vue
@@ -1,4 +1,4 @@
-<!-- This is the login view. Users can login by entering a correct username and password -->
+<!-- This is the login view. Users can login by entering a correct email and password -->
 <template>
   <div class="container">
     <div class="row justify-content-center align-items-center">
@@ -6,14 +6,15 @@
         <form @submit.prevent="login">
           <h2 class="text-center">Login</h2>
           <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" v-model="username" class="form-control" id="username" placeholder="Enter username">
+            <label for="email">Email</label>
+            <input type="text" v-model="email" class="form-control" id="email" placeholder="Enter Email">
           </div>
           <div class="form-group">
             <label for="password">Password</label>
             <input type="password" v-model="password" class="form-control" id="password" placeholder="Enter password">
           </div>
           <button type="submit" class="btn btn-primary btn-block ">Login</button>
+          <p id="status_display"></p>
         </form>
       </div>
     </div>
@@ -27,29 +28,41 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      username: '',
+      email: '',
       password: '',
     };
   },
   methods: {
     async login() {
       try {
-        // Send a POST request to your login API with username and password
-        //const response = await axios.post('your_login_api_url', {
-        //  username: this.username,
-        //  password: this.password,
-       // });
-
-        // Assuming your API responds with a token upon successful login
-        //const token = response.data.token;
+        // Send a POST request to your login API with email and password
+        const response = await axios.post(import.meta.env.VITE_ROOT_API+ '/login', {
+          email: this.email,
+          password: this.password,
+        });
+          // Assuming your API responds with a token upon successful login
+        const token = response.data.token;
+          const user = response.data.user;
 
         // Store the token in local storage or a Vuex store for authentication
+        localStorage.setItem('token', token);
+         localStorage.setItem('user',JSON.stringify(user));
 
+        if(user.role == 'employee')
         // Redirect to HomeView.vue or any other route
-        this.$router.push('/');
+          this.$router.push('/');
+        else{
+           const statusDisplay = document.getElementById('status_display');
+        statusDisplay.textContent = 'Should be redirected to admin page';
+        }
+
+
+        
       } catch (error) {
         // Handle login error
         console.error('Login failed:', error);
+        const statusDisplay = document.getElementById('status_display');
+        statusDisplay.textContent = error.response.data.message;
         // You can display an error message to the user if needed
       }
     },
