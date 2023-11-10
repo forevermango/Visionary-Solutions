4a13866b45e7f04dab85f711a06f2f4c04bf622a
pmpham3 <98196805+pmpham3@users.noreply.github.com>
2023-11-10 03:36:07 -0600

    Update app.js

--- Visionary-Solutions/Backend/app.js
+++ Visionary-Solutions/Backend/app.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 const db = require('./config/db')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:8080", "https://todaysvision.onrender.com"]
+}));
 app.use(bodyParser.json());
 app.use(morgan('tiny'))
 
