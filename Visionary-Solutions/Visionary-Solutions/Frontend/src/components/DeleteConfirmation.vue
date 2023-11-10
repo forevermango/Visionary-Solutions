fc8fa9c7aebe52146d1574732c36335eb1e68c0f
pmpham3 <98196805+pmpham3@users.noreply.github.com>
2023-11-09 22:16:55 -0600

    Create DeleteConfirmation.vue for delete customer delete

---
+++ Visionary-Solutions/Visionary-Solutions/Frontend/src/components/DeleteConfirmation.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="deletion-confirmation-backdrop">
+    <div class="deletion-confirmation">
+      <p>Are you sure you want to delete the user?</p>
+      <div class="confirmation-buttons">
+        <button type="submit" @click="confirmDeletion" class="btn btn-danger">Yes, Delete</button>
+        <button @click="cancelDeletion" class="btn btn-secondary">Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+  
+<script>
+export default {
+  methods: {
+    confirmDeletion() {
+      this.$emit('confirm');
+    },
+    cancelDeletion() {
+      this.$emit('cancel');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  margin: 0 10px;
+}
+  .deletion-confirmation-backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .deletion-confirmation {
+    text-align: center;
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+  }
+
+  .confirmation-buttons {
+    margin-top: 20px;
+  }
+</style>
+
